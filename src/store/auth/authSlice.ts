685aafb4a41227816ba87688cb2bef068600b5bd
Artiquee <amex7708@gmail.com>
2024-10-12 21:40:21 +0300

    feat: add extraReducers for login

--- src/store/auth/authSlice.ts
+++ src/store/auth/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { register } from "./authThunk";
+import { register, login } from "./authThunk";
 import { DataStatus } from "@/common/enums/app/DataStatus";
 
 type User = {
@@ -36,6 +36,20 @@ export const authSlice = createSlice({
             state.user = action.payload;
           })
           .addCase(register.rejected, (state: IAuthState) => {
+            state.isLoading = false;
+            state.status = DataStatus.Error;
+          })
+
+          .addCase(login.pending, (state: IAuthState) => {
+            state.isLoading = true;
+            state.status = DataStatus.Pending;
+          })
+          .addCase(login.fulfilled, (state: IAuthState, action) => {
+            state.isLoading = false;
+            state.status = DataStatus.Success;
+            state.user = action.payload;
+          })
+          .addCase(login.rejected, (state: IAuthState) => {
             state.isLoading = false;
             state.status = DataStatus.Error;
           })
